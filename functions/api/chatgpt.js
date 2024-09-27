651301558a0b8364c77e1925b01f1c3be8b901f2
qyzhizi <2830942138@qq.com>
2024-09-27 21:01:03 +0800

    first commit

---
+++ functions/api/chatgpt.js
@@ -0,0 +1,35 @@
+export async function onRequestPost(context) {
+    const { request, env } = context;
+    
+    const text = request.text;
+    const openai_key = env.OpenAIKey
+    // const input = "你的输入内容"; // 替换为实际输入内容
+
+    const requestData = {
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: text }],
+    };
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${openai_key}`, // 使用模板字面量插入变量
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+    };
+    
+    fetch('https://burn.hair/v1/chat/completions', requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json(); // 解析 JSON 响应
+        })
+        .then(data => {
+            // 处理成功的响应数据
+        })
+        .catch(error => {
+            // 处理错误
+        });
+}
